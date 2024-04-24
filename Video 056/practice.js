f9222d802e71d86777b6a3bc03f4b9530c720893
Asmit Phuyal <asmitphuyal4040@gmail.com>
2024-04-24 11:25:54 +0545

    conditionals and operators

---
+++ Video 056/practice.js
@@ -0,0 +1,35 @@
+let age = 13;
+
+// if(age>=10 && age<=20){
+//     console.log("age is between 10 and 20");
+// }
+
+// else{
+//     console.log("age is not between 10 and 20");
+// }
+
+
+// switch(age){
+//     case 1:{
+//         console.log("age is 1");
+//         break;
+//     }
+
+//     case 10:{
+//         console.log("age is 10");
+//         break;
+//     }
+
+//     case 13:{
+//         console.log("age is 13");
+//         break;
+//     }
+//     default:{
+//         console.log("enter valid age");
+//     }
+// }
+
+
+
+
+
