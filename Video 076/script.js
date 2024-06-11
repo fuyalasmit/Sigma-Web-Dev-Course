fc9f148ceac7dbed3d5ad0f6ac94d0e1e7b1b2f5
Asmit Phuyal <asmitphuyal4040@gmail.com>
2024-06-11 22:22:48 +0545

    Async Await & Fetch API

---
+++ Video 076/script.js
@@ -0,0 +1,99 @@
+// async function getData() {
+//     // Simulate getting data from a server
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             resolve(455);
+//         }, 3500);
+//     })
+// }
+
+// async function getData() {
+//     // Simulate getting data from a server
+//     let x = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+//     let data = await x.json();
+//     console.log(data);
+// }
+
+// async function main(){
+//     console.log("Loading modules");
+
+//     console.log("Do something else");
+
+//     console.log("Load data");
+
+//     let data = await getData();
+
+//     console.log(data);
+
+//     console.log("process data");
+
+//     console.log("task 2");
+// }
+
+// main();
+
+ 
+
+// data.then((v) => { 
+//     console.log(data)
+
+//     console.log("process data")
+
+//     console.log("task 2")
+ 
+// })
+
+
+
+
+/*Async Await*/
+
+// async function hello(){
+//     console.log("Hello");
+// }
+
+// function api() {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             console.log("Weather Data");
+//             resolve(200);
+//         }, 2000);
+//     })
+// }
+// async function getWeatherData(){
+//     await api(); //1st call
+//     await api(); //2nd call
+
+// }
+
+
+function getData(dataId) {
+    return new Promise ((resolve,reject)=>{
+        setTimeout(() => {
+            console.log("data is: ", dataId);
+            resolve("success");
+        }, 2000);
+    });
+}
+
+async function getAllData(){
+    console.log("Getting Data 1...");
+    await getData(1);
+    console.log("Getting Data 2...");
+    await getData(2);
+    console.log("Getting Data 3...");
+    await getData(3);
+    console.log("Getting Data 4...");
+    await getData(4);
+}
+
+(async function (){
+    console.log("Getting Data 1...");
+    await getData(1);
+    console.log("Getting Data 2...");
+    await getData(2);
+    console.log("Getting Data 3...");
+    await getData(3);
+    console.log("Getting Data 4...");
+    await getData(4);
+})();
