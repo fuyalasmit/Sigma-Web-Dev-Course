9c8bf9046d82f2e739cea9380d7766eaf5cd3744
Asmit Phuyal <asmitphuyal4040@gmail.com>
2024-06-12 08:29:33 +0545

    Async Await & Fetch API

--- Video 076/script.js
+++ Video 076/script.js
@@ -67,33 +67,63 @@
 // }
 
 
-function getData(dataId) {
-    return new Promise ((resolve,reject)=>{
-        setTimeout(() => {
-            console.log("data is: ", dataId);
-            resolve("success");
-        }, 2000);
-    });
+// function getData(dataId) {
+//     return new Promise ((resolve,reject)=>{
+//         setTimeout(() => {
+//             console.log("data is: ", dataId);
+//             resolve("success");
+//         }, 2000);
+//     });
+// }
+
+// async function getAllData(){
+//     console.log("Getting Data 1...");
+//     await getData(1);
+//     console.log("Getting Data 2...");
+//     await getData(2);
+//     console.log("Getting Data 3...");
+//     await getData(3);
+//     console.log("Getting Data 4...");
+//     await getData(4);
+// }
+
+// (async function (){
+//     console.log("Getting Data 1...");
+//     await getData(1);
+//     console.log("Getting Data 2...");
+//     await getData(2);
+//     console.log("Getting Data 3...");
+//     await getData(3);
+//     console.log("Getting Data 4...");
+//     await getData(4);
+// })();
+
+
+/* fetch api*/
+const URL = "https://cat-fact.herokuapp.com/facts";
+const factPara = document.querySelector("#fact");
+const button = document.getElementById("btn");
+
+
+const getFacts = async () => {
+    console.log("Getting Data...");
+    let response = await fetch(URL);
+    console.log(response);  //json format
+    // console.log(response["status"]);  
+    let data = await response.json();
+    // console.log(data[0].text);
+    factPara.innerText = data[2].text;
 }
 
-async function getAllData(){
-    console.log("Getting Data 1...");
-    await getData(1);
-    console.log("Getting Data 2...");
-    await getData(2);
-    console.log("Getting Data 3...");
-    await getData(3);
-    console.log("Getting Data 4...");
-    await getData(4);
-}
 
-(async function (){
-    console.log("Getting Data 1...");
-    await getData(1);
-    console.log("Getting Data 2...");
-    await getData(2);
-    console.log("Getting Data 3...");
-    await getData(3);
-    console.log("Getting Data 4...");
-    await getData(4);
-})();
+// function getFacts(){
+//     let promise = fetch(URL);
+//     promise.then((response)=>{
+//         return response.json();
+//     }).then((data)=>{
+//         console.log(data);
+//         factPara.innerText = data[2].text;
+//     });
+// }
+
+button.addEventListener("click", getFacts);
